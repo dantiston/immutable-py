23160c8db5fc90e820f79e202ddf7f2febde30ff
T.J. Trimble <trimblet@me.com>
2020-08-02 01:49:44 -0700

    Initialization, Statics, writes, and gets

--- immutable/immutable.py
+++ immutable/immutable.py
@@ -1,63 +1,99 @@
 #!/usr/bin/env python3
 
-from typing import Iterable, Generic, Tuple, TypeVar, Union
+from typing import Iterable, Iterator, Callable, Generic, Tuple, TypeVar
 
 KeyT = TypeVar('KeyT')
 ValueT = TypeVar('ValueT')
-NotSetValueT = TypeVar('NotSetValueT')
+ReturnT = TypeVar('ReturnT')
 
 singleton = object()
 
 class Collection(Generic[ValueT]):
     pass
 
-class Sequence(Collection, Generic[ValueT]):
-    pass
+class Indexed(Collection, Generic[ValueT]):
 
-class List(Sequence, Generic[ValueT]):
+    def has(self, index: int) -> bool:
+        return index < len(self)
+
+    def first(self, nsv: ValueT = singleton) -> ValueT:
+        return self.get(0, nsv)
+
+    def last(self, nsv: ValueT = singleton) -> ValueT:
+        return self.get(-1, nsv)
+
+    def __contains__(self, value: ValueT) -> bool:
+        return self.includes(value)
+
+class List(Indexed, Generic[ValueT]):
+
     def __init__(self, items: Iterable[ValueT] = ()) -> None:
         self.items = list(items)
 
     @classmethod
     def of(cls, *items: Tuple[ValueT]) -> 'List':
-        return List(items)
+        return List(items) if items else _empty_list
 
     @classmethod
     def is_list(cls, other) -> bool:
         return isinstance(other, List)
 
-    def get(self, index: int) -> ValueT:
-        return self.items[index]
-
-    def has(self, index: int) -> bool:
-        return index < len(self.items)
+    def get(self, index: int, nsv: ValueT = singleton) -> ValueT:
+        try:
+            return self.items[index]
+        except IndexError as e:
+            if nsv is singleton:
+                raise e
+            return nsv
 
     def includes(self, value: ValueT) -> bool:
         return value in self.items
 
-    def first(self, nsv: NotSetValueT = singleton) -> Union[ValueT, NotSetValueT]:
-        try:
-            return self.items[0]
-        except IndexError as e:
-            if nsv is singleton:
-                raise e
-            return nsv
-
-    def last(self, nsv: NotSetValueT = singleton) -> Union[ValueT, NotSetValueT]:
-        try:
-            return self.items[-1]
-        except IndexError as e:
-            if nsv is singleton:
-                raise e
-            return nsv
+    def set(self, index: int, value: ValueT) -> 'List':
+        result = self.items[:]
+        result[index] = value
+        return List(result)
 
     def delete(self, index: int) -> 'List':
         result = self.items[:]
         result.pop(index)
-        return result
+        return List(result)
+
+    def insert(self, index: int, value: ValueT) -> 'List':
+        result = self.items[:]
+        result.insert(index, value)
+        return List(result)
+
+    def push(self, *values: Tuple[ValueT]) -> 'List':
+        result = self.items[:]
+        result.extend(values)
+        return List(result)
+
+    def pop(self, *values: Tuple[ValueT]) -> 'List':
+        return self.delete(-1)
+
+    def unshift(self, *values: Tuple[ValueT]) -> 'List':
+        result = self.items[:]
+        return List(list(values) + result)
+
+    def shift(self, *values: Tuple[ValueT]) -> 'List':
+        result = self.items[:]
+        return List(result + list(values))
+
+    def update(self, index: int, updater: Callable[[ValueT], ValueT], nsv: ValueT = singleton) -> 'List':
+        return self.set(index, updater(self.get(index, nsv)))
+
+    def update_all(self, updater: Callable[['List'], ReturnT]) -> ReturnT:
+        return List(updater(self.items[:]))
+
+    def clear(self) -> 'List':
+        return _empty_list
 
     def __len__(self) -> int:
         return len(self.items)
 
-    def __contains__(self, value: ValueT) -> bool:
-        return self.includes(value)
+    def __iter__(self) -> Iterator[ValueT]:
+        return iter(self.items)
+
+
+_empty_list = List()
--- tests/test_immutable.py
+++ tests/test_immutable.py
@@ -15,6 +15,10 @@ class TestImmutableList(unittest.TestCase):
         self.assertEqual(a.get(1), 2)
         self.assertEqual(a.get(2), 3)
 
+    def test_of_empty(self):
+        a = immutable.List.of()
+        self.assertEqual(len(a), 0)
+
     def test_of_get(self):
         a = immutable.List.of(1, 2, 3)
         self.assertEqual(a.get(0), 1)
@@ -28,6 +32,10 @@ class TestImmutableList(unittest.TestCase):
     def test_is_list_positive(self):
         a = immutable.List.of(1, 2, 3)
         self.assertTrue(immutable.List.is_list(a))
+
+    def test_get_nsv(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(a.get(3, 4), 4)
 
     def test_includes(self):
         a = immutable.List((1, 2, 3))
@@ -59,11 +67,106 @@ class TestImmutableList(unittest.TestCase):
         a = immutable.List()
         self.assertEqual(a.last(0), 0)
 
+    def test_set(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.set(1, 7)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 7, 3])
+
     def test_delete(self):
         a = immutable.List((1, 2, 3))
-        self.assertEqual(a.get(2), 3)
-        self.assertEqual(len(a), 3)
+        self.assertEqual(list(a), [1, 2, 3])
         b = a.delete(2)
-        self.assertEqual(a.get(2), 3)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 2])
+
+    def test_insert(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.insert(1, 4)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 4, 2, 3])
+
+    def test_clear(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.clear()
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [])
+
+    def test_push_one(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.push(4)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 2, 3, 4])
+
+    def test_push_many(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.push(4, 5, 6)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 2, 3, 4, 5, 6])
+
+    def test_pop(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.pop()
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 2])
+
+    def test_unshift_one(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.unshift(0)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [0, 1, 2, 3])
+
+    def test_unshift_many(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
         self.assertEqual(len(a), 3)
-        self.assertEqual(len(b), 2)
+        b = a.unshift(0, 1, 2)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [0, 1, 2, 1, 2, 3])
+
+    def test_shift_one(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.shift(0)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 2, 3, 0])
+
+    def test_shift_many(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.shift(0, 1, 2)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 2, 3, 0, 1, 2])
+
+    def test_update(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.update(1, lambda x: x * 4)
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [1, 8, 3])
+
+    def test_update_all(self):
+        a = immutable.List((1, 2, 3))
+        self.assertEqual(list(a), [1, 2, 3])
+        b = a.update_all(lambda values: map(lambda x: x * 2, values))
+        self.assertEqual(list(a), [1, 2, 3])
+        self.assertEqual(list(b), [2, 4, 6])
+
+    def test_len(self):
+        a = immutable.List((1, 2, 3))
+        actual = len(a)
+        expected = 3
+        self.assertEqual(actual, expected)
+
+    def test_iter(self):
+        a = immutable.List((1, 2, 3))
+        actual = list(a)
+        expected = [1, 2, 3]
+        self.assertEqual(actual, expected)
