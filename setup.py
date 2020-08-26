c960844d560fddfa72c97aad0e80728a2cac270e
T.J. Trimble <trimblet@me.com>
2020-08-25 22:32:36 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="immutable.py",
+    version="0.1",
+    description="A library providing many Persistent Immutable data structures based on Immutable.js",
+    author="T.J. Trimble",
+    author_email="trimblet@me.com",
+    url="https://github.com/dantiston/immutable-py",
+    packages=["immutable"],
+)
